f6b97f26612c58a1e268d7945c65ef22eff984c5
Arnór Ýmir Guðjósson <arnorymir@Arnors-MacBook-Pro-2.local>
2014-09-12 13:50:20 +0000

    implemented Cellpath class

---
+++ app/src/main/java/com/example/flowflow/freeflow/Cellpath.java
@@ -0,0 +1,36 @@
+package com.example.flowflow.freeflow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by arnorymir on 12/09/14.
+ */
+public class Cellpath {
+
+    private ArrayList<Coordinate> mPath = new ArrayList<Coordinate>();
+
+    public void append(Coordinate coordinate) {
+        int index = mPath.indexOf(coordinate);
+        if (index >= 0) {
+            for (int i=mPath.size()-1; i > index; --i) {
+                mPath.remove(i);
+            }
+        }
+        else {
+            mPath.add(coordinate);
+        }
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return mPath;
+    }
+
+    public void reset() {
+        mPath.clear();
+    }
+
+    public boolean isEmpty() {
+        return mPath.isEmpty();
+    }
+}
