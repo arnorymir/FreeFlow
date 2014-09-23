ac6651d69a39127723bf447e443b129800059c68
Arnór Ýmir Guðjósson <arnorymir@Arnors-MacBook-Pro-2.local>
2014-09-23 21:13:54 +0000

    added dbhelper and puzzlesadapter

---
+++ app/src/main/java/com/example/flowflow/freeflow/DbHelper.java
@@ -0,0 +1,39 @@
+package com.example.flowflow.freeflow;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "PUZZLES_DB";
+    public static final int DB_VERSION = 1;
+
+    public static final String TablePuzzle = "puzzles";
+    public static final String[] TablePuzzleCols = { "puzzleId", "moves", "finished"};
+
+    private static final String sqlCreateTablePuzzles =
+            "CREATE TABLE puzzles(" +
+                    " puzzleId INTEGER PRIMARY KEY NOT NULL," +
+                    " moves INTEGER," +
+                    " finished INTEGER " +
+                    ");";
+
+    private static final String sqlDropTablePuzzles =
+            "DROP TABLE IF EXISTS puzzles;";
+
+    public DbHelper( Context context ) {
+        super( context, DB_NAME, null, DB_VERSION );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL( sqlCreateTablePuzzles );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL( sqlDropTablePuzzles );
+        onCreate( db );
+    }
+}
---
+++ app/src/main/java/com/example/flowflow/freeflow/PuzzlesAdapter.java
@@ -0,0 +1,101 @@
+package com.example.flowflow.freeflow;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class PuzzlesAdapter {
+
+    SQLiteDatabase db;
+    DbHelper dbHelper;
+    Context context;
+
+    public PuzzlesAdapter( Context c ) {
+        context = c;
+    }
+
+    public PuzzlesAdapter openToRead() {
+        dbHelper = new DbHelper( context );
+        db = dbHelper.getReadableDatabase();
+        return this;
+    }
+
+    public PuzzlesAdapter openToWrite() {
+        dbHelper = new DbHelper( context );
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public long insertPuzzle( int id, int moves , boolean finished ) {
+        String[] cols = DbHelper.TablePuzzleCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(cols[0], id);
+        contentValues.put(cols[1], moves);
+        contentValues.put(cols[2], finished ? "1":"0");
+        openToWrite();
+        long value = db.insert(DbHelper.TablePuzzle, null, contentValues);
+        close();
+        return value;
+    }
+
+    public long updatePuzzle(int id, int moves, boolean finished ) {
+        String[] cols = DbHelper.TablePuzzleCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( cols[1], moves);
+        contentValues.put( cols[2], finished ? "1" : "0" );
+        openToWrite();
+        long value = db.update(DbHelper.TablePuzzle,
+                contentValues,
+                cols[1] + "=" + id, null );
+        close();
+        return value;
+    }
+
+    // sets all puzzles to unfinished but keeps the best move for
+    // each puzzle
+    public long resetAllPuzzlesFinished() {
+        String[] cols = DbHelper.TablePuzzleCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( cols[2], "0");
+        openToWrite();
+        long value = db.update(DbHelper.TablePuzzle,
+                contentValues,
+                null, null );
+        close();
+        return value;
+    }
+
+    //set all puzzles to unfinished and removes all best moves.
+    public long resetAllPuzzlesFinishedAndMove() {
+        String[] cols = DbHelper.TablePuzzleCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( cols[1], "null");
+        contentValues.put( cols[2], "0");
+        openToWrite();
+        long value = db.update(DbHelper.TablePuzzle,
+                contentValues,
+                null, null );
+        close();
+        return value;
+    }
+
+    public Cursor queryStudents() {
+        openToRead();
+        Cursor cursor = db.query( DbHelper.TablePuzzle,
+                DbHelper.TablePuzzleCols, null, null, null, null, null);
+        return cursor;
+    }
+
+    public Cursor queryStudent( int id) {
+        openToRead();
+        String[] cols = DbHelper.TablePuzzleCols;
+        Cursor cursor = db.query( DbHelper.TablePuzzle,
+                cols, cols[0] + "=" + id, null, null, null, null);
+        return cursor;
+    }
+}
